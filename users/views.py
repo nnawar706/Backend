923de94537cb55355e46d338ba192128b236ca85
nnawar706 <nnawar706@gmail.com>
2023-09-01 21:51:42 +0600

    user

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
-from rest_frameworks import APIView, permissions, status
+from rest_framework.views import APIView
+from rest_framework import permissions, status
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class RegisterView(APIView):
+class UserRegistrationView(APIView):
     def post(self, request):
         data = request.data
 
@@ -17,6 +18,7 @@ class RegisterView(APIView):
         return JsonResponse({
             'status': True
         }, status = status.HTTP_201_CREATED)
+
 
 class RetrieveUsersView(APIView):
     permission_classes = [permissions.IsAuthenticated]
