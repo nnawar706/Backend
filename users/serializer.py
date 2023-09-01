c41b383bb90e668b81acb659823506542ea762cf
nnawar706 <nnawar706@gmail.com>
2023-09-01 22:33:06 +0600

    user-registration

--- users/serializer.py
+++ users/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 
 User = get_user_model()
 
@@ -15,6 +16,10 @@ class UserCreateSerializer (serializers.ModelSerializer):
         if len(name) < 5:
             raise serializers.ValidationError('Name field must be at least 5 characters long.')
         
+        try:
+            validate_password(data['password'])
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError(serializers.as_serializer_error(e)['non_field_errors'])
 
         return data
     
