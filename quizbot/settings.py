9b78eed1d8fd8acb89971e286bc731048d75b812
nnawar706 <nnawar706@gmail.com>
2023-09-14 00:37:23 +0600

    cors

--- quizbot/settings.py
+++ quizbot/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    # "rest_framework_simplejwt",
+    "corsheaders",
     "users",
     "rooms",
     "quizzes"
@@ -54,6 +54,14 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://192.168.0.169:3000',
 ]
 
 ROOT_URLCONF = "quizbot.urls"
