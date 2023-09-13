0a44ac257ad71d2a40c8ed60a16655ed7c49daa8
nnawar706 <nnawar706@gmail.com>
2023-09-13 13:26:46 +0600

    quizzes

--- quizzes/views.py
+++ quizzes/views.py
@@ -2,21 +2,29 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework import permissions
+from rooms.models import ExamRoom
 from .permissions import *
 from .serializer import *
 
 class QuizCreateView (APIView):
     permission_classes = [permissions.IsAuthenticated, IsTeacher]
 
-    def post (self, request):
+    def post (self, request, room_id):
         data = request.data
 
-        serializer = QuizCreateSerializer(data = data, context={'request': request})
+        try:
+            room = ExamRoom.objects.get(pk=room_id)
+        except ExamRoom.DoesNotExist:
+            return JsonResponse({
+            'status': False,
+            'error': 'Room not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = QuizCreateSerializer(data = data, context={'request': request, 'room': room})
 
         if not serializer.is_valid():
             return JsonResponse({
                 'status': False,
-                'error': validation_error(serializer.errors)
+                'error': serializer.errors
             }, status = status.HTTP_422_UNPROCESSABLE_ENTITY)
 
         quiz = serializer.create(serializer.validated_data)
