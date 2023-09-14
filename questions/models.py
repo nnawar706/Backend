c006a8ca54238d29d0c6419b005de8b7b4b0de9a
nnawar706 <nnawar706@gmail.com>
2023-09-14 11:33:51 +0600

    questions

---
+++ questions/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from quizzes.models import Quiz
+
+class QuestionType(models.Model):
+    name           = models.CharField(max_length=50, unique = True)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    quiz            = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_type   = models.ForeignKey(QuestionType, on_delete=models.CASCADE)
+    detail          = models.CharField(max_length=300, null=True, blank=True)
+    total           = models.FloatField()
+
+
+class SubQuestion(models.Model):
+    question        = models.ForeignKey(Question, on_delete=models.CASCADE)
+    image_url       = models.CharField(max_length=100, null=True, blank=True)
+    ques            = models.CharField(max_length=1000)
+    sub_mark        = models.FloatField()
+
+
+class SubQuestionAnswer(models.Model):
+    sub_question        = models.ForeignKey(SubQuestion, on_delete=models.CASCADE)
+    answer              = models.CharField(max_length=1000)
+    status              = models.BooleanField()
--- quizbot/settings.py
+++ quizbot/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     "corsheaders",
     "users",
     "rooms",
-    "quizzes"
+    "quizzes",
+    "questions"
 ]
 
 MIDDLEWARE = [
