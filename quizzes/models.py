2010eb99b140febd33f76b76521d5558cc39b348
nnawar706 <nnawar706@gmail.com>
2023-09-20 11:54:18 +0600

    quiz-update

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from rooms.models import ExamRoom
+from django.conf import settings
 
-class Quiz(models.Model):
+class Quiz (models.Model):
     room            = models.ForeignKey(ExamRoom, on_delete=models.CASCADE, related_name='quizzes')
     title           = models.CharField(max_length=50)
     occurring_date  = models.DateField()
@@ -15,3 +16,15 @@ class Quiz(models.Model):
 
     def __str__(self):
         return self.room.title + ' - ' + self.title
+
+
+class QuizMark (models.Model):
+    quiz          = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='quiz')
+    student       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='examinee')
+    obtained_mark = models.FloatField()
+
+    class Meta:
+        unique_together = ('quiz', 'student')
+
+    def __str__(self):
+        return self.quiz.room.title + ' - ' + self.quiz.title + ' - ' + self.obtained_mark
