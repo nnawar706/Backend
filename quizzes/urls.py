4d18e02ed83a62c4d116023580ebdb1257fc91ab
nnawar706 <nnawar706@gmail.com>
2023-10-30 20:10:07 +0600

    quiz-marks

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("<int:room_id>", QuizCreateView.as_view()),
     path("all/<int:room_id>", QuizView.as_view()),
     path("update/<int:pk>", QuizUpdateView.as_view()),
-    path("get_marks/<int:pk>", QuizMarksView.as_view())
+    path("publish_marks/<int:pk>", QuizMarksView.as_view())
 ]
